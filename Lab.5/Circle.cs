468d38520b27fbc4d181d24341167c6e08f05fcb
Shakejelly <fredrich93@gmail.com>
2023-10-18 11:15:51 +0200

    Add project files.

---
+++ Lab.5/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._5
+{
+    public class Circle
+    {
+     
+            public  void GetArea()
+            {
+                Console.WriteLine("Skriv in radie i cm till en cirkel så visar jag dess area");
+                double radius = Convert.ToInt32(Console.ReadLine());
+
+                double area = radius * radius * Math.PI;
+                Console.WriteLine(area);
+
+            }
+        }
+    }
+
---
+++ Lab.5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+
+            circle.GetArea();
+        }
+    }
+}
